ee1df5512daa0d991910188fe80091e89e7251c0
ParanoidAndroid19 <simranbhake.sb@gmail.com>
2021-10-02 13:25:38 -0500

    buy and sell

--- frontend/src/pages/User/index.js
+++ frontend/src/pages/User/index.js
@@ -7,9 +7,30 @@ import {
   MenuItem,
   FormControl,
   Select,
+  TextField,
+  Button
 } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  buyButton: {
+    backgroundColor: "#428c14",
+    color: "white",
+    fontWeight: "bold",
+    '&:hover': {
+      backgroundColor: "#346b12",
+      color: "white",
+    }
+  },
+  sellButton: {
+    backgroundColor: "#ce2727",
+    color: "white",
+    fontWeight: "bold",
+    '&:hover': {
+      backgroundColor: "#992020",
+      color: "white",
+    }
+  }
+}));
 
 export default function User() {
   const classes = useStyles();
@@ -27,40 +48,78 @@ export default function User() {
   };
 
   return (
-    <div style={{ marginTop: "20px", marginLeft: "20px", display: "flex", justifyContent: "space-evenly" }}>
-      <Box sx={{ minWidth: 120, maxWidth: 150 }}>
-        <FormControl variant="outlined" fullWidth>
-          <InputLabel id="demo-simple-select-label">State</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={state}
-            label="state"
-            onChange={handleChange}
-          >
-            <MenuItem value="tx">Texas</MenuItem>
-            <MenuItem value="ca">California</MenuItem>
-            <MenuItem value="az">Arizona</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
+    <div style={{ textAlign: "center" }}>
+      <div
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          justifyContent: "space-evenly",
+          marginBottom: "60px",
+        }}
+      >
+        <Box sx={{ minWidth: 120, maxWidth: 150 }}>
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel id="demo-simple-select-label">State</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={state}
+              label="state"
+              onChange={handleChange}
+            >
+              <MenuItem value="tx">Texas</MenuItem>
+              <MenuItem value="ca">California</MenuItem>
+              <MenuItem value="az">Arizona</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
 
-      <Box sx={{ minWidth: 120, maxWidth: 150 }}>
-        <FormControl variant="outlined" fullWidth>
-          <InputLabel id="demo-simple-select-label">Pool</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={pool}
-            label="pool"
-            onChange={handlePool}
-          >
-            <MenuItem value={1}>Pool 1</MenuItem>
-            <MenuItem value={2}>Pool 2</MenuItem>
-            <MenuItem value={3}>Pool 3</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
+        <Box sx={{ minWidth: 120, maxWidth: 150 }}>
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel id="demo-simple-select-label">Pool</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={pool}
+              label="pool"
+              onChange={handlePool}
+            >
+              <MenuItem value={1}>Pool 1</MenuItem>
+              <MenuItem value={2}>Pool 2</MenuItem>
+              <MenuItem value={3}>Pool 3</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <div
+          style={{
+            border: "solid 1px darkgrey",
+            minWidth: "20%",
+            padding: "20px",
+            display: "grid"
+          }}
+        >
+          <p>Buy</p>
+
+          <TextField id="outlined-basic" label="Quantity" variant="outlined" style={{marginBottom: "20px"}}/> 
+          <Button className={classes.buyButton} variant="contained">Buy</Button>
+        </div>
+        <div
+          style={{
+            border: "solid 1px darkgrey",
+            minWidth: "20%",
+            padding: "20px",
+            display: "grid"
+          }}
+        >
+          <p>Sell</p>
+
+          <TextField id="outlined-basic" label="Quantity" variant="outlined" style={{marginBottom: "20px"}}/> 
+          <Button className={classes.sellButton} variant="contained">Sell</Button>
+        </div>
+      </div>
     </div>
   );
 }
